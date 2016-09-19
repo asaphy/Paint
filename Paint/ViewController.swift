577a2947c7bac300cf061219da10955f575b4642
Asaph <asaph@intrepid.io>
2016-09-19 14:39:41 -0400

    Initial Commit

---
+++ Paint/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Paint
+//
+//  Created by Asaph Yuan on 9/19/16.
+//  Copyright © 2016 Asaph Yuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
